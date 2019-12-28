9367b45e757ca6f184a4848ec1ad8df73aea87ea
mithunrana <mithunrana.developer@gmail.com>
2019-12-29 01:45:07 +0600

    porfile setup compleate

--- app/Http/Controllers/AboutController.php
+++ app/Http/Controllers/AboutController.php
@@ -1,7 +1,6 @@
 <?php
 
 namespace App\Http\Controllers;
-use Illuminate\Support\Facades\DB;
 use Illuminate\Http\Request;
 use App\AboutProfile;
 class AboutController extends Controller {
@@ -20,7 +19,7 @@ class AboutController extends Controller {
         //$users = DB::table('about_profiles')->get();
         $users = AboutProfile::first();
         if(isset($users->id)){
-            $Profile = AboutProfile::find($request->id);
+            $Profile = AboutProfile::find($users->id);
             $Profile->Name = $request->Name;
             $Profile->ShortBrief = $request->ShortBrief;
             $Profile->Details = $request->Details;
@@ -46,8 +45,35 @@ class AboutController extends Controller {
             $Profile->Github_Link = $request->Github_Link;
             $Profile->Google_Map = $request->Google_Map;
             $Profile->stackoverflow_link = $request->stackoverflow_link;
+            $Profile->save();
         }else{
-            echo "get result not found";
+            $Profile = new  AboutProfile();
+            $Profile->Name = $request->Name;
+            $Profile->ShortBrief = $request->ShortBrief;
+            $Profile->Details = $request->Details;
+            $Profile->Discover_link = $request->Discover_link;
+            $Profile->Download_link = $request->Download_link;
+            $Profile->Email1 = $request->Email1;
+            $Profile->Email2 = $request->Email2;
+            $Profile->Phone1 = $request->Phone1;
+            $Profile->Phone2 = $request->Phone2;
+            $Profile->Address = $request->Address;
+            $Profile->Facebook_Link = $request->Facebook_Link;
+            $Profile->Youtube_Link = $request->Youtube_Link;
+            $Profile->Twitter_Link = $request->Twitter_Link;
+
+            $Profile->Linkedin_Link = $request->Linkedin_Link;
+            $Profile->Fiveer_Link = $request->Fiveer_Link;
+            $Profile->Upwork_Link = $request->Upwork_Link;
+            $Profile->Instagram_Link = $request->Instagram_Link;
+            $Profile->Pinterest_Link = $request->Pinterest_Link;
+            $Profile->Behance_Link = $request->Behance_Link;
+            $Profile->Dribble_Link = $request->Dribble_Link;
+            $Profile->Vimeo_Link = $request->Vimeo_Link;
+            $Profile->Github_Link = $request->Github_Link;
+            $Profile->Google_Map = $request->Google_Map;
+            $Profile->stackoverflow_link = $request->stackoverflow_link;
+            $Profile->save();
         }
     }
 
--- resources/views/Admin/profile.blade.php
+++ resources/views/Admin/profile.blade.php
@@ -27,29 +27,157 @@
 
             <!-- Main row -->
             <div class="row">
+                <form method="POST" action="{{url('profile-update')}}">
+                @csrf
+                <input type="hidden" class="form-control" value="" id="image1" name="image1">
+                <input type="hidden" class="form-control" value="" id="image2" name="image2">
+                <input type="hidden" class="form-control" value="" id="setImageAltText" name="imageAltText">
+                <input type="hidden" class="form-control" value="" id="setImageTitleText" name="imageTitleText">
+
                 <div class="col-sm-12">
-                    <div class="col-sm-3">
-                        <div style="max-width:300px;">
-                            <img style="max-width: 100%;" src="images/img_lights.jpg"/>
-                        </div>
+                    <div class="col-sm-4">
+                        <img style="width: 100%;" id="firstimage" src="images/img_lights.jpg" >
+                        <p style="width:100%; border-top-left-radius:0px;border-top-right-radius:0px;"  data-toggle="modal" data-target="#imagemodal" class="btn btn-primary">Select Image</p>
                     </div>
-                    <div class="col-sm-9">
-                        <form action="/action_page.php">
+                    <div class="col-sm-8">
                             <div class="form-group">
-                                <label for="email">Email address:</label>
-                                <input type="email" class="form-control" id="email">
+                                <label for="name">Name:</label>
+                                <input type="text" class="form-control" value="" name="Name" id="Name">
                             </div>
                             <div class="form-group">
-                                <label for="pwd">Password:</label>
-                                <input type="password" class="form-control" id="pwd">
+                                <label for="email1">Email One:</label>
+                                <input type="text" class="form-control" value="" name="Email1" id="email1">
                             </div>
-                            <div class="checkbox">
-                                <label><input type="checkbox"> Remember me</label>
+                            <div class="form-group">
+                                <label for="email2">Email Two:</label>
+                                <input type="text" class="form-control" value="" name="Email2" id="email2">
                             </div>
-                            <button type="submit" class="btn btn-default">Submit</button>
-                        </form>
                     </div>
                 </div>
+                    <div class="col-sm-12">
+                        <div class="col-sm-12">
+                            <div class="form-group">
+                                <label for="shortbrief">Short Brief:</label>
+                                <textarea type="text" rows="6" class="form-control" name="ShortBrief" id="shortbrief"></textarea>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-sm-12">
+                        <div class="col-sm-8">
+                                <div class="form-group">
+                                    <label for="phone1">Phone 1:</label>
+                                    <input type="text" class="form-control" name="Phone1" id="phone1">
+                                </div>
+                                <div class="form-group">
+                                    <label for="phone2">Phone 2:</label>
+                                    <input type="text" class="form-control" name="Phone2" id="phone2">
+                                </div>
+                                <div class="form-group">
+                                    <label for="address">Address:</label>
+                                    <textarea type="text" rows="2" class="form-control" name="Address" id="address"></textarea>
+                                </div>
+                        </div>
+                        <div class="col-sm-4">
+                            <img style="width: 100%;" id="Previewimage2" src="images/img_lights.jpg"/>
+                            <p style="width:100%; border-top-left-radius:0px;border-top-right-radius:0px;"  data-toggle="modal" data-target="#imagemodal" class="btn btn-primary">Select Image</p>
+                        </div>
+                    </div>
+                    <div class="col-sm-12">
+                        <div class="form-group col-sm-6">
+                            <label for="name">Discover Link:</label>
+                            <input type="text" class="form-control" name="Discover_link" id="name">
+                        </div>
+
+                        <div class="form-group col-sm-6">
+                            <label for="name">Resume Download Link:</label>
+                            <input type="text" class="form-control" name="Download_link" id="name">
+                        </div>
+
+                        <div class="form-group col-sm-6">
+                            <label for="name">Facebook Link:</label>
+                            <input type="text" class="form-control" name="Facebook_Link" id="name">
+                        </div>
+
+                        <div class="form-group col-sm-6">
+                            <label for="name">Youtube Link:</label>
+                            <input type="text" class="form-control" name="Youtube_Link" id="name">
+                        </div>
+
+                        <div class="form-group col-sm-6">
+                            <label for="name">Twitter Link:</label>
+                            <input type="text" class="form-control" name="Twitter_Link" id="name">
+                        </div>
+
+                        <div class="form-group col-sm-6">
+                            <label for="name">Linked in Link:</label>
+                            <input type="text" class="form-control" name="Linkedin_Link" id="name">
+                        </div>
+
+                        <div class="form-group col-sm-6">
+                            <label for="name">Fiverr Link:</label>
+                            <input type="text" class="form-control" name="Fiveer_Link" id="name">
+                        </div>
+
+                        <div class="form-group col-sm-6">
+                            <label for="name">Upwork Link:</label>
+                            <input type="text" class="form-control" name="Upwork_Link" id="name">
+                        </div>
+
+                        <div class="form-group col-sm-6">
+                            <label for="name">Instagram Link:</label>
+                            <input type="text" class="form-control" name="Instagram_Link" id="name">
+                        </div>
+
+                        <div class="form-group col-sm-6">
+                            <label for="name">Pinterest Link:</label>
+                            <input type="text" class="form-control" name="Pinterest_Link" id="name">
+                        </div>
+
+                        <div class="form-group col-sm-6">
+                            <label for="name">Behance Link:</label>
+                            <input type="text" class="form-control" name="Behance_Link" id="name">
+                        </div>
+
+                        <div class="form-group col-sm-6">
+                            <label for="name">Dribble Link:</label>
+                            <input type="text" class="form-control" name="Dribble_Link" id="name">
+                        </div>
+
+                        <div class="form-group col-sm-6">
+                            <label for="name">Vimeo Link:</label>
+                            <input type="text" class="form-control" name="Vimeo_Link" id="name">
+                        </div>
+
+                        <div class="form-group col-sm-6">
+                            <label for="name">Github Link:</label>
+                            <input type="text" class="form-control" name="Github_Link" id="name">
+                        </div>
+
+                        <div class="form-group col-sm-6">
+                            <label for="name">Google Map:</label>
+                            <input type="text" class="form-control" name="Google_Map" id="name">
+                        </div>
+
+                        <div class="form-group col-sm-6">
+                            <label for="name">Stack Overflow:</label>
+                            <input type="text" class="form-control" name="stackoverflow_link" id="name">
+                        </div>
+                    </div>
+                    <div class="col-sm-12">
+                        <div class="col-sm-12">
+                            <div class="form-group">
+                                <label for="Details">Details:</label>
+                                <textarea type="text" rows="6" class="form-control" name="Details" id="Details"></textarea>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-sm-12">
+                        <div class="col-sm-12">
+                            <input type="submit" value="UPDATE" class="btn btn-success" >
+                        </div>
+                    </div>
+
+                </form>
             </div>
         </section>
     </div>
@@ -70,10 +198,19 @@
             var imageid = $('#getimageId').val();
             var imagealttext = $('#getImageAltText').val();
             var imagetitletext = $('#getImageTitleText').val();
-            $('#setimageid').attr('value',imageid);
+            var imageurl = $('#imagelocation').val();
+            $('#firstimage').attr('src',imageurl);
+            $('#image1').attr('value',imageid);
             $('#setImageAltText').attr('value',imagealttext);
             $('#setImageTitleText').attr('value',imagetitletext);
         })
+
+        $('#iconselect').click(function(){
+            var imageid = $('#getimageId').val();
+            var imageurl = $('#imagelocation').val();
+            $('#image2').attr('value',imageid);
+            $('#Previewimage2').attr('src',imageurl);
+        });
     });
 </script>
 
